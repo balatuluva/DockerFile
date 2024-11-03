1ecaf606ba928277110712cae9e052ccb1070c23
balatuluva <bala.tulva@gmail.com>
2024-11-03 10:53:48 +0000

    cluster admin steps

--- RBAC/steps.sh
+++ RBAC/steps.sh
@@ -5,3 +5,7 @@ openssl x509 -req -in user1.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out us
 openssl genrsa -out user2.key 2048
 openssl req -new -key user2.key -out user2.csr -subj "/CN=user2/O=production"
 openssl x509 -req -in user2.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out user2.crt -days 365
+
+openssl genrsa -out bala.key 2048
+openssl req -new -key bala.key -out bala.csr -subj "/CN=bala/O=clusteradmin"
+openssl x509 -req -in bala.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out bala.crt -days 365
